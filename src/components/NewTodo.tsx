95b83c952f442c4a970e93d665262aa0659288bf
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-12-26 14:16:45 +0000

    Creating Form to add new Todo that receives a function in props

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from "react";
+
+const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props)  => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (event: React.FormEvent) =>{
+        event.preventDefault();
+        const enteredText = todoTextInputRef.current!.value;
+        
+        if (enteredText.trim().length === 0 ){
+            console.log("Text is mandatory");
+            return;
+        }
+        props.onAddTodo(enteredText);
+
+    }
+
+  return (
+    <form onSubmit={submitHandler}>
+        <label htmlFor="text"> Todo Text</label>
+        <input type='text' id="text" ref={todoTextInputRef}/>
+        <button type="submit">Add</button>
+    </form>
+  )
+}
+
+export default NewTodo;
