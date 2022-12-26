11932427da1bec5f1bd0313196902d97f685bc12
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-12-26 11:32:35 +0000

    Adding a Todo component that expects a specific props with an array of strings and the default children. This is achieved by enhacing React.FC with the items array. (There should be a better way to do this)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div>
-            
+      <Todos items={['Learn React', 'Learn Typescript']} />            
     </div>
   );
 }
