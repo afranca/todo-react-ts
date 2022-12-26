2265189601341f1815e6342d1e6cf0d7097372a8
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-12-26 14:34:38 +0000

    Adding useState with Typescript syntax to update the todo list.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
+
+  const [todos, setTodos] = useState<Todo[]>([]);
   
-  const todos: Todo[] = [ 
-    new Todo('Learn React'), 
-    new Todo('Learn Typescript')
-  ];
+  const addNewTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos( 
+      (prev) => {
+        //return prev.concat(newTodo);
+        return [...prev, newTodo]; //prev.concat(newTodo);
+      }
+    );
+  }
 
   return (
     <div>
+      <NewTodo onAddTodo={addNewTodoHandler}/>
       <Todos items={todos} />            
     </div>
   );
